8b95174a9362dbc231bbf84f87675fc892f75f92
Dileep-Kotha <kothadileep2002@gmail.com>
2025-05-09 14:39:46 +0530

    Add files via upload

---
+++ SingletonProject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance=null;
+        private Singleton()
+        {
+
+        }
+        public static Singleton GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+        public void display()
+        {
+            Console.WriteLine("Singleton Pattern");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s=Singleton.GetInstance();
+            s.display();
+            //Singleton s = Singleton.GetInstance();
+            //s.display();
+
+        }
+    }
+}
---
+++ factory_pattern.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public interface Shape
+    {
+        void draw();
+    }
+    public class Rectangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Rectangle shape");
+        }
+
+    }
+    public class Circle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Circle shape");
+        }
+
+    }
+    public class ShapeFactory
+    {
+        
+        public Shape GetShape(string sh)
+        {
+            if (sh == null)
+            {
+                return null;
+            }
+            else if (sh.Equals("CIRCLE", StringComparison.OrdinalIgnoreCase)) // Use StringComparison for case-insensitive comparison
+            {
+                return new Circle();
+            }
+            else if (sh.Equals("RECTANGLE", StringComparison.OrdinalIgnoreCase)) // Added logic for Rectangle
+            {
+                return new Rectangle();
+            }
+            return null; // Default return if the shape type is not recognized
+        }
+       
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory shapeFactory = new ShapeFactory();
+
+            Shape s1 = shapeFactory.GetShape("CIRCLE");
+            s1.draw();
+            Shape s2 = shapeFactory.GetShape("RECTANGLE");
+            s2.draw();
+
+
+
+        }
+    }
+}
