0ae05e4eb3fbbb44d7840f519532abf12a29ece7
Dileep-Kotha <kothadileep2002@gmail.com>
2025-05-09 16:29:34 +0530

    Add files via upload

---
+++ Command_Pattern.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Pattern
+{
+    public interface command
+    {
+        void Execute();
+    }
+    class Lighton : command
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Light On");
+        }
+    }
+    class Lightoff : command
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Light OFF");
+        }
+    }
+    public class Remote//Invoker
+    {
+        public void pressbutton(command c)
+        {
+            c.Execute();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)//Client
+        {
+            Remote remote = new Remote();
+            remote.pressbutton(new Lighton());
+            remote.pressbutton(new Lightoff());
+
+
+        }
+    }
+}
---
+++ Proxy_Pattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Pattern
+{
+    internal class Program
+    {
+        public interface IBankserver//interface
+        {
+            void withdraw(int amount);
+        }
+        public class RBankserver:IBankserver//Real object
+        {
+            public void  withdraw(int amount)
+            {
+                Console.WriteLine($"{amount} of money is withdrawed");
+            }
+        }
+        public class Proxy : IBankserver//proxy it verifies and provides security
+        {
+            public RBankserver r;
+            public void withdraw(int amount) {
+                if(r == null)
+                {
+                    r = new RBankserver();
+                }
+                Console.WriteLine("validating");
+                r.withdraw(amount);
+            }
+            
+
+        }
+        static void Main(string[] args)
+        {
+            IBankserver i = new RBankserver();//creating  bank object
+            i.withdraw(5000);
+        }
+    }
+}
